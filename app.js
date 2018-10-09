8c417def4da8014c7c0dbed0db6a5d30923aab50
Andrew Kamal <kamalandrew55@gmail.com>
2018-10-09 01:02:41 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
 console.log('# CryptoExchange v0.1 starting up #');
-
 var Exchange = require('./lib/Exchange.js');
 var config = require('/root/lol/config.json');
-
 exchange = new Exchange(config);
 process.exchange = exchange;
-
 exchange.wallet.daemons.LTC.getInfo(function(err, info) {
 	console.log('# Litecoin Status #');
 	if (err) throw err;
 	else console.log(info.blocks, info.connections);
 });
-
 exchange.wallet.daemons.DOGE.getInfo(function(err, info) {
 	console.log('# Dogecoin Status #');
 	if (err) throw err;
 	else console.log(info.blocks, info.connections);
 });
-
-exchange.wallet.daemons.RPC.getInfo(function(err, info) {
+exchange.wallet.daemons.LNR.getInfo(function(err, info) {
 	console.log('# Ronpaulcoin Status #');
 	if (err) throw err;
 	else console.log(info.blocks, info.connections);
